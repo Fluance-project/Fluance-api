1bbf75a21c2fd2d0e50d7b86517f5089ceba0e82
Saidi Tayeb <saidi.tayeb@hotmail>
2020-06-19 22:43:00 +0200

    first init

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, jsonify, request, session, render_template, make_response
+from functools import wraps
+import jwt
+import datetime
+import services.verification as vf
+import json
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Fluance'
+
+def check_for_token(func):
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        token = request.headers['Authorization'].replace('Bearer ', '')
+        if not token:
+            return jsonify({'message': 'Missing token'}), 403
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except:
+            return jsonify({'message' : 'Invalid token'}), 403
+        return func(*args, **kwargs)
+    return wrapped
+
+@app.route('/')
+def index():
+    print(request.headers['Authorization'].replace('Bearer ', ''))
+    return ''
+    # return 'hi, for now we use /login and /register only'
+
+@app.route('/login', methods=['GET'])
+def login():
+    if request.headers['Content-Type'] == 'application/json':
+        rq = json.loads(request.data)
+        if set(rq.keys()) == {'email', 'password'}:
+            if vf.verify_password(rq):
+                session['logged_in'] = True
+                token = jwt.encode({
+                    'user': rq['email'],
+                    'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=10)
+                },
+                app.config['SECRET_KEY'])
+                return jsonify({'token': token.decode('utf-8')})
+            else:
+                return make_response('User or password wrong', 403, {'WWW-Authenticate': 'Basic realm="Login required"'})
+        return jsonify({'message': 'Good form required'}), 406
+
+@app.route('/register', methods=['POST'])
+def register():
+    if request.headers['Content-Type'] == 'application/json':
+        status = vf.account_check(json.loads(request.data))
+        if status== 406:
+            return jsonify({'message': 'Good form required'}), 406
+        if status == 409:
+            return jsonify({'message': 'Email already exist'}), 409
+
+        return jsonify({'message': 'account added to the database'}), 200
+
+@app.route('/auth')
+@check_for_token
+def authorised():
+    """
+    This page has been created to test the token only
+    :return:
+    """
+    return 'page protected'
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ services/verification.py
@@ -0,0 +1,88 @@
+from pymongo import MongoClient
+import hashlib
+import os
+import binascii
+import uuid
+
+
+
+def hash_password(password):
+    """
+    Hash a password for storing.
+    :param password: string
+    :return: string
+    """
+    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'),
+                                salt, 100000)
+    pwdhash = binascii.hexlify(pwdhash)
+    return (salt + pwdhash).decode('ascii')
+
+def verify_password(account):
+    """
+    Verify a stored password against one provided by user
+    :param account: json
+    :return: boolean (true if the password is valid)
+    """
+    client = MongoClient('mongodb://root:root@localhost:27017/')
+    db = client.fluance
+
+    if isNotNewEmail(account['email'], db):
+        provided_password = account['password']
+        stored_password = db.accounts.find_one({'email': account['email']})['password']
+        salt = stored_password[:64]
+        stored_password = stored_password[64:]
+        pwdhash = hashlib.pbkdf2_hmac('sha512',
+                                      provided_password.encode('utf-8'),
+                                      salt.encode('ascii'),
+                                      100000)
+        pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+        return pwdhash == stored_password
+
+def account_check(account):
+    """
+    check if the json recived is in the good format
+    :param account:
+    :return:
+    """
+    if KeysVerif(account):
+        return 406
+
+    client = MongoClient('mongodb://root:root@localhost:27017/')
+    db = client.fluance
+
+    if isNotNewEmail(account['email'], db):
+        return 409
+
+    # hash password
+    account['password'] = hash_password(account['password'])
+    db.accounts.insert_one(account).inserted_id
+    return 200
+
+
+# def KeysVerif(list_):
+#     """
+#     Check if all keys are correct
+#     :param list_:
+#     :return:
+#     """
+#     if {x for x in list_.keys()} == {'companyName', 'email', 'user', 'password'} and\
+#             len(list_['user']) > 0 and {x for x in list_['user'][0].keys()} == {'first name', 'last name', 'role', 'title'}:
+#         return False
+#     return True
+
+def KeysVerif(list_):
+    """
+    Check if all keys are correct
+    :param list_:
+    :return:
+    """
+    if {x for x in list_.keys()} == {'companyName', 'email', 'password'}:
+        return False
+    return True
+
+
+def isNotNewEmail(name, db):
+    if db.accounts.find_one({"email": name}) is not None:
+        return True
+    return False
