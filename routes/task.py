8a17a3f792070f3e09fb84961ace80d25c1c808b
Saidi Tayeb <saidi.tayeb@hotmail.com>
2020-06-26 11:24:30 +0200

    Fixes #15

--- routes/task.py
+++ routes/task.py
@@ -5,6 +5,7 @@ import json
 from bson import json_util, ObjectId
 import services.task as st
 
+
 @app.route('/api/v1/task/by-account/<account_id>', methods=['GET'])
 @check_for_token
 def getTaskForAccount(account_id):
@@ -16,6 +17,7 @@ def getTaskForAccount(account_id):
             return json_util.dumps(req), 200
     else:
         return jsonify({'error': 'Please use application/json as content type'}), 422
+
 
 @app.route('/api/v1/task/by-machine/<machine_id>', methods=['GET'])
 @check_for_token
@@ -29,6 +31,7 @@ def getTaskForMachine(machine_id):
     else:
         return jsonify({'error': 'Please use application/json as content type'}), 422
 
+
 @app.route('/api/v1/task/<task_id>', methods=['GET'])
 @check_for_token
 def getTask(task_id):
@@ -41,6 +44,7 @@ def getTask(task_id):
     else:
         return jsonify({'error': 'Please use application/json as content type'}), 422
 
+
 @app.route('/api/v1/task', methods=['POST'])
 @check_for_token
 def addTask():
@@ -49,6 +53,7 @@ def addTask():
         return jsonify({'message': 'task has been added'}), 200
     else:
         return jsonify({'error': 'Please use application/json as content type'}), 422
+
 
 @app.route('/api/v1/task/<task_id>', methods=['PATCH', 'PUT'])
 @check_for_token
@@ -62,6 +67,7 @@ def editTask(task_id):
     else:
         return jsonify({'error': 'Please use application/json as content type'}), 422
 
+
 @app.route('/api/v1/task/<task_id>', methods=['DELETE'])
 @check_for_token
 def deleteTask(task_id):
@@ -71,3 +77,44 @@ def deleteTask(task_id):
 
         return jsonify({'message': 'task has been deleted'}), 200
 
+
+@app.route('/api/v1/task/<task_id>/comment', methods=['POST'])
+@check_for_token
+def addComment(task_id):
+    if request.headers['Content-Type'] == 'application/json':
+            status = st.add_comment(task_id, json.loads(request.data))
+            if status== 409:
+                return jsonify({'message': 'task id not found'}), 406
+
+            return jsonify({'message': 'comment has been added'}), 200
+    else:
+        return jsonify({'error': 'Please use application/json as content type'}), 422
+
+
+@app.route('/api/v1/task/<task_id>/comment/<comment_id>', methods=['GET'])
+@check_for_token
+def getComment(task_id, comment_id):
+    req = st.get_comment(comment_id)
+    if req == 409:
+        return jsonify({'message': 'comment id not found'}), 409
+    return json_util.dumps(req), 200
+
+@app.route('/api/v1/task/<task_id>/comment/<comment_id>', methods=['PUT', 'PATCH'])
+@check_for_token
+def updateComment(task_id, comment_id):
+    if request.headers['Content-Type'] == 'application/json':
+        req = st.update_comment(comment_id, json.loads(request.data))
+        if req == 409:
+            return jsonify({'message': 'comment id not found'}), 409
+        return jsonify({'message': 'comment has been updated'}), 200
+    else:
+        return jsonify({'error': 'Please use application/json as content type'}), 422
+
+@app.route('/api/v1/task/<task_id>/comment/<comment_id>', methods=['DELETE'])
+@check_for_token
+def deleteComment(task_id, comment_id):
+        status = st.remove_comment(task_id, comment_id)
+        if status== 409:
+            return jsonify({'message': 'task id not found'}), 406
+
+        return jsonify({'message': 'task has been deleted'}), 200
