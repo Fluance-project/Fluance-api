86269399bd83c885ba4382e011d839bb06ba9a59
Saidi Tayeb <saidi.tayeb@hotmail.com>
2020-07-01 23:52:54 +0200

    remove test files

--- test.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_pymongo import PyMongo
-
-app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://root:root@54.92.140.66:27017/fluance"
-mongo = PyMongo(app)
-online_users = mongo.db.accounts.insert_one()
